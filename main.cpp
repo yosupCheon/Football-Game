b0f7ffd28e14811933d015b761c21e21579bc2e4
Yosup Cheon <cheonyo0409@gmail.com>
2022-08-19 03:20:10 -0700

    edit main to create teams

--- main.cpp
+++ main.cpp
@@ -11,42 +11,81 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
+/* demo versino
+    - TEAM 1: 4-3-3
+    - TEAM 2: 3-5-2
+*/
+
 int main () {
-    // create home team
+    // CREATE HOME TEAM (4-3-3)
     Team team1 ("Team 1");
-    //age att def gok
-    Player vdS ("VDS", "GK", 35, 10, 20, 84); //GK
-    Player nV ("Vidic", "CB", 30, 30, 90, 45); //CB
-    Player jsP ("Park", "CM", 32, 80, 80, 20); //CM
-    Player wR ("Rooney", "ST", 33, 94, 33, 10); //ST
+    // GOAL KEEPER [age att def gok]
+    Player t1GK ("t1GK", "GK", 35, 10, 20, 84);
+    // CENTER BACK
+    Player t1CB1 ("t1CB1", "CB", 30, 35, 80, 25);
+    Player t1CB2 ("t1CB2", "CB", 24, 35, 80, 25);
+    Player t1CB3 ("t1CB3", "CB", 28, 35, 80, 25);
+    Player t1CB4 ("t1CB4", "CB", 25, 35, 80, 25);
+    // MIDFIELDER
+    Player t1MF1 ("t1MF1", "MF", 27, 60, 60, 20);
+    Player t1MF2 ("t1MF2", "MF", 25, 60, 60, 20);
+    Player t1MF3 ("t1MF3", "MF", 25, 60, 60, 20);
+    // STRIKER
+    Player t1ST1 ("t1ST1", "ST", 29, 80, 20, 10);
+    Player t1ST2 ("t1ST2", "ST", 30, 80, 20, 10);
+    Player t1ST3 ("t1ST3", "ST", 23, 80, 20, 10);
 
-    // create away team
+    // CREATE AWAYS TEAM (3-5-2)
     Team team2 ("Team 2");
-    Player pC ("Cecb", "GK", 34, 10, 15, 85); //GK
-    Player jT ("Terry", "CB", 31, 25, 90, 40); //CB
-    Player nK ("Kante", "CM", 28, 85, 85, 10); //CM
-    Player dD ("Drogba", "ST", 34, 98, 20, 10); //ST
+    // GOAL KEEPER
+    Player t2GK ("t2GK", "GK", 33, 10, 20, 81);
+    // CENTER BACK
+    Player t2CB1 ("t2CB1", "CB", 21, 40, 70, 25);
+    Player t2CB2 ("t2CB2", "CB", 30, 30, 90, 25);
+    Player t2CB3 ("t2CB3", "CB", 24, 40, 80, 25);
+    // MIDFIELDER
+    Player t2MF1 ("t2MF1", "MF", 22, 60, 60, 20);
+    Player t2MF2 ("t2MF2", "MF", 27, 60, 60, 20);
+    Player t2MF3 ("t2MF3", "MF", 29, 60, 60, 20);
+    Player t2MF4 ("t2MF4", "MF", 32, 60, 60, 20);
+    Player t2MF5 ("t2MF5", "MF", 30, 60, 60, 20);
+    // STRIKER
+    Player t2ST1 ("t2ST1", "ST", 35, 85, 15, 10);
+    Player t2ST2 ("t2ST1", "ST", 35, 58, 15, 10);
 
-    //add players to the team
-    team1.addPlayer(vdS);
-    team1.addPlayer(nV);
-    team1.addPlayer(jsP);
-    team1.addPlayer(wR);
+    //ADD PLAYERS TO THE TEAM
+    // TEAM 1
+    team1.addPlayer(t1GK);
+    team1.addPlayer(t1CB1);
+    team1.addPlayer(t1CB2);
+    team1.addPlayer(t1CB3);
+    team1.addPlayer(t1CB4);
+    team1.addPlayer(t1MF1);
+    team1.addPlayer(t1MF2);
+    team1.addPlayer(t1MF3);
+    team1.addPlayer(t1ST1);
+    team1.addPlayer(t1ST2);
+    team1.addPlayer(t1ST3);
     //team1.playersGetter();
-    //cout << team1.overAllGetter() << endl;
-    //cout << endl;
-    team2.addPlayer(pC);
-    team2.addPlayer(jT);
-    team2.addPlayer(nK);
-    team2.addPlayer(dD);
+    cout << team1.overAllGetter() << endl;
+    
+    // TEAM 2
+    team2.addPlayer(t2GK);
+    team2.addPlayer(t2CB1);
+    team2.addPlayer(t2CB2);
+    team2.addPlayer(t2CB3);
+    team2.addPlayer(t2MF1);
+    team2.addPlayer(t2MF2);
+    team2.addPlayer(t2MF3);
+    team2.addPlayer(t2MF4);
+    team2.addPlayer(t2MF5);
+    team2.addPlayer(t2ST1);
+    team2.addPlayer(t2ST2);
     //team2.playersGetter();
-    //cout << team2.overAllGetter() << endl;
+    cout << team2.overAllGetter() << endl;
 
-    //game simulate
-    // first parameter is home (adds +5 to the overall)
-    // the result is return by the team's player's overall
-    Simulation round1 (team1, team2);
-    Simulation round2 (team2, team1);
+    Simulation game (team1, team2);
+    cout <<"game over in main" << endl;
 
     return 0;
 }
