2942587a4eb4837b5601efc346b406b3cf74e81a
Yosup Cheon <cheonyo0409@gmail.com>
2022-08-24 00:06:50 -0700

    added to-do in main

--- main.cpp
+++ main.cpp
@@ -11,9 +11,27 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
-/* demo versino
+/*
+What to do Next
+- make a function that turn to default formation (4-3-3)
+- depends on the tactic, move players
+- organize codes
+*/
+
+/* default formation
     - TEAM 1: 4-3-3
-    - TEAM 2: 3-5-2
+    - TEAM 2: 4-3-3
+*/
+
+/*
+defense
+5-3-2
+
+balance
+4-3-3
+
+offensive
+3-3-4
 */
 
 int main () {
@@ -35,7 +53,7 @@ int main () {
     Player t1ST2 ("t1ST2", "ST", 30, 80, 20, 10);
     Player t1ST3 ("t1ST3", "ST", 23, 80, 20, 10);
 
-    // CREATE AWAYS TEAM (3-5-2)
+    // CREATE AWAYS TEAM (4-3-3)
     Team team2 ("Team 2");
     // GOAL KEEPER
     Player t2GK ("t2GK", "GK", 33, 10, 20, 81);
@@ -43,15 +61,15 @@ int main () {
     Player t2CB1 ("t2CB1", "CB", 21, 40, 70, 25);
     Player t2CB2 ("t2CB2", "CB", 30, 30, 90, 25);
     Player t2CB3 ("t2CB3", "CB", 24, 40, 80, 25);
+    Player t2CB4 ("t2CB4", "CB", 32, 20, 65, 20);
     // MIDFIELDER
     Player t2MF1 ("t2MF1", "MF", 22, 60, 60, 20);
     Player t2MF2 ("t2MF2", "MF", 27, 60, 60, 20);
     Player t2MF3 ("t2MF3", "MF", 29, 60, 60, 20);
-    Player t2MF4 ("t2MF4", "MF", 32, 60, 60, 20);
-    Player t2MF5 ("t2MF5", "MF", 30, 60, 60, 20);
     // STRIKER
     Player t2ST1 ("t2ST1", "ST", 35, 85, 15, 10);
-    Player t2ST2 ("t2ST1", "ST", 35, 58, 15, 10);
+    Player t2ST2 ("t2ST2", "ST", 35, 58, 15, 10);
+    Player t2ST3 ("t2ST3", "ST", 30, 80, 10, 20);
 
     //ADD PLAYERS TO THE TEAM
     // TEAM 1
@@ -74,13 +92,13 @@ int main () {
     team2.addPlayer(t2CB1);
     team2.addPlayer(t2CB2);
     team2.addPlayer(t2CB3);
+    team2.addPlayer(t2CB4);
     team2.addPlayer(t2MF1);
     team2.addPlayer(t2MF2);
     team2.addPlayer(t2MF3);
-    team2.addPlayer(t2MF4);
-    team2.addPlayer(t2MF5);
     team2.addPlayer(t2ST1);
     team2.addPlayer(t2ST2);
+    team2.addPlayer(t2ST3);
     //team2.playersGetter();
     //cout << team2.overAllGetter() << endl;
 
