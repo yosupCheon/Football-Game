706b97c02f4c473a316b0caf901fc6b4766807b0
Yosup Cheon <cheonyo0409@gmail.com>
2022-08-27 18:44:13 -0700

    Update for managing players using  Linked List

--- main.cpp
+++ main.cpp
@@ -11,99 +11,66 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
-/*
-What to do Next
-- make a function that turn to default formation (4-3-3)
-- depends on the tactic, move players
-- organize codes
-*/
-
-/* default formation
-    - TEAM 1: 4-3-3
-    - TEAM 2: 4-3-3
-*/
-
-/*
-defense
-5-3-2
-
-balance
-4-3-3
-
-offensive
-3-3-4
+/* FORMATION
+default 4-3-3
+defense 5-3-2
+balance 4-3-3
+offence 3-3-4
 */
 
 int main () {
-    // CREATE HOME TEAM (4-3-3)
-    Team team1 ("Team 1");
-    // GOAL KEEPER [age att def gok]
-    Player t1GK ("t1GK", "GK", 35, 10, 20, 84);
+    // [name, position, age, gok, def, att]
+    Team team1("Team1");
+    // GK
+    team1.addPlayer ("t1GK", "GK", 35, 84, 20, 10);
     // CENTER BACK
-    Player t1CB1 ("t1CB1", "CB", 30, 35, 70, 25);
-    Player t1CB2 ("t1CB2", "CB", 24, 35, 70, 25);
-    Player t1CB3 ("t1CB3", "CB", 28, 35, 70, 25);
-    Player t1CB4 ("t1CB4", "CB", 25, 35, 70, 25);
-    // MIDFIELDER
-    Player t1MF1 ("t1MF1", "MF", 27, 60, 60, 20);
-    Player t1MF2 ("t1MF2", "MF", 25, 60, 60, 20);
-    Player t1MF3 ("t1MF3", "MF", 25, 60, 60, 20);
-    // STRIKER
-    Player t1ST1 ("t1ST1", "ST", 29, 80, 20, 10);
-    Player t1ST2 ("t1ST2", "ST", 30, 80, 20, 10);
-    Player t1ST3 ("t1ST3", "ST", 23, 80, 20, 10);
+    team1.addPlayer ("t1CB1", "CB", 30, 25, 90, 35);
+    team1.addPlayer ("t1CB2", "CB", 24, 25, 70, 35);
+    team1.addPlayer ("t1CB3", "CB", 28, 25, 71, 35);
+    team1.addPlayer ("t1CB4", "CB", 25, 25, 70, 35);
+    // MIDFIELDERS
+    team1.addPlayer ("t1MF1", "MF", 27, 20, 60, 60);
+    team1.addPlayer ("t1MF2", "MF", 25, 20, 62, 60);
+    team1.addPlayer ("t1MF3", "MF", 25, 20, 60, 61);
+    // STRIKERS
+    team1.addPlayer ("t1ST1", "ST", 29, 10, 20, 80);
+    team1.addPlayer ("t1ST2", "ST", 30, 10, 20, 80);
+    team1.addPlayer ("t1ST3", "ST", 23, 10, 20, 85);
 
-    // CREATE AWAYS TEAM (4-3-3)
-    Team team2 ("Team 2");
-    // GOAL KEEPER
-    Player t2GK ("t2GK", "GK", 33, 10, 20, 81);
-    // CENTER BACK
-    Player t2CB1 ("t2CB1", "CB", 21, 40, 70, 25);
-    Player t2CB2 ("t2CB2", "CB", 30, 30, 90, 25);
-    Player t2CB3 ("t2CB3", "CB", 24, 40, 80, 25);
-    Player t2CB4 ("t2CB4", "CB", 32, 20, 65, 20);
-    // MIDFIELDER
-    Player t2MF1 ("t2MF1", "MF", 22, 60, 60, 20);
-    Player t2MF2 ("t2MF2", "MF", 27, 60, 60, 20);
-    Player t2MF3 ("t2MF3", "MF", 29, 60, 60, 20);
-    // STRIKER
-    Player t2ST1 ("t2ST1", "ST", 35, 85, 15, 10);
-    Player t2ST2 ("t2ST2", "ST", 35, 58, 15, 10);
-    Player t2ST3 ("t2ST3", "ST", 30, 80, 10, 20);
+    Team team2 ("Team2");
+    //GK
+    team2.addPlayer ("t2GK", "GK", 33, 81, 20, 10);
+    //CB
+    team2.addPlayer ("t2CB1", "CB", 21, 25, 70, 40);
+    team2.addPlayer ("t2CB2", "CB", 30, 25, 70, 30);
+    team2.addPlayer ("t2CB3", "CB", 24, 25, 80, 40);
+    team2.addPlayer ("t2CB4", "CB", 32, 20, 60, 20);
+    //MF
+    team2.addPlayer ("t2MF1", "MF", 22, 20, 65, 60);
+    team2.addPlayer ("t2MF2", "MF", 27, 20, 60, 65);
+    team2.addPlayer ("t2MF3", "MF", 29, 20, 60, 60);
+    //ST
+    team2.addPlayer ("t2ST1", "ST", 35, 10, 15, 85);
+    team2.addPlayer ("t2ST2", "ST", 35, 10, 15, 70);
+    team2.addPlayer ("t2ST3", "ST", 30, 20, 10, 80);
 
-    //ADD PLAYERS TO THE TEAM
-    // TEAM 1
-    team1.addPlayer(t1GK);
-    team1.addPlayer(t1CB1);
-    team1.addPlayer(t1CB2);
-    team1.addPlayer(t1CB3);
-    team1.addPlayer(t1CB4);
-    team1.addPlayer(t1MF1);
-    team1.addPlayer(t1MF2);
-    team1.addPlayer(t1MF3);
-    team1.addPlayer(t1ST1);
-    team1.addPlayer(t1ST2);
-    team1.addPlayer(t1ST3);
-    //team1.playersGetter();
-    //cout << team1.overAllGetter() << endl;
-    
-    // TEAM 2
-    team2.addPlayer(t2GK);
-    team2.addPlayer(t2CB1);
-    team2.addPlayer(t2CB2);
-    team2.addPlayer(t2CB3);
-    team2.addPlayer(t2CB4);
-    team2.addPlayer(t2MF1);
-    team2.addPlayer(t2MF2);
-    team2.addPlayer(t2MF3);
-    team2.addPlayer(t2ST1);
-    team2.addPlayer(t2ST2);
-    team2.addPlayer(t2ST3);
-    //team2.playersGetter();
-    //cout << team2.overAllGetter() << endl;
-
-    Simulation game (team1, team2);
+    Simulation game;
+    char startMenu = game.startFunction();
+    if (startMenu == '1') {
+        game.gameLoop(team1, team2);
+    }
+    else if (startMenu == '2') {
+        cout << "CREATE A PLAYER..." << endl;
+    }
+    else if (startMenu == '3') {
+        cout << "CREATE A TEAM..." << endl;
+    }
+    else if (startMenu == 'q') {
+        cout << "BYE!!!" << endl;
+        return 0;
+    }
+    cout << endl;
     cout <<"game over in main" << endl;
 
     return 0;
-}
+} 
